114156cc1b53c2e4aa38d1a66cca0747433b82a9
rrezxth <rienz.edu@icloud.com>
2024-05-30 19:30:11 -0400

    Prototype: Display food items functioning

--- src/app.js
+++ src/app.js
@@ -38,3 +38,52 @@ app.listen(PORT, () => console.log(`Listening in PORT: ${PORT}`));
 app.get("/", (req, res) => {
     res.render('index');
 });
+/*
+app.get("/order", (req, res) => {
+    res.render('appOrder');
+});
+*/
+app.get("/order", (req, res) => {
+
+    // TODO: Should be from database (using mongoose)
+    const dummyFoods = [
+        {
+            _id: "1",
+            dname: "Gravy Chicken",
+            dprice: "12.99",
+            photo: "gravy_chicken.jpg"
+        },
+        {
+            _id: "2",
+            dname: "Chow Mein (Singaporean Noodles)",
+            dprice: "12.99",
+            photo: "chow_mein.jpg"
+        },
+        {
+            _id: "3",
+            dname: "Squid Calamari",
+            dprice: "13.99",
+            photo: "squid_calamari.jpeg"
+        },
+        {
+            _id: "4",
+            dname: "Spring Rolls (Veggies)",
+            dprice: "8.99",
+            photo: "spring_rolls_veggies.jpg"
+        },
+        {
+            _id: "5",
+            dname: "Assorted Fried Rice *House Special*",
+            dprice: "15.99",
+            photo: "assorted_fried_rice.jpg"
+        },
+        {
+            _id: "6",
+            dname: "Beef with Brocolli",
+            dprice: "13.99",
+            photo: "beef_broccoli.jpg"
+        }
+    ];
+
+    res.render('appOrder', { foods: dummyFoods });
+});
