22130b2e6669627b0f84e7d59b4271e99a01e316
rrezxth <rienz.edu@icloud.com>
2024-06-26 13:08:23 -0400

    fixed db connection

--- src/database/dbconfig.js
+++ src/database/dbconfig.js
@@ -1,11 +1,13 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../../.env' });
 const mongoose = require('mongoose');
 const databaseUrl = process.env.DATABASE_URL;
 
 async function connectToMongo() {
     try {
         await mongoose.connect(databaseUrl);
+        console.log('MongoDB Connected..');
     } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
         process.exit(1);
     }
 }
