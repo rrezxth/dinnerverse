74f584dc2e4f1bca27f118bc3166171fe788e1d8
rrezxth <rienz.edu@icloud.com>
2024-06-26 19:08:18 -0400

    MongoDB connection created and implemented app.post for /login

--- src/app.js
+++ src/app.js
@@ -2,12 +2,22 @@
 // CONFIGURATIONS
 // ==============================
 const express = require('express');
+const app = express();
 const path = require('path');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 
-const app = express();
-//const {connectToMongo} = require("./dbConfig");
+//const jwt = require('jsonwebtoken');
+//const jwtSecret = process.env.JWT_SECRET;
+//const cookieParser = require('cookie-parser');
+
+
+//// IMPORT from directory
+require('dotenv').config({ path: '../.env' });
+const {connectToMongo} = require("./database/dbconfig");
+const User = require('./database/model/userModel');
 
 // ==============================
 // MIDDLEWARES
@@ -15,6 +25,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 
 app.engine(".hbs", exphbs.engine({
     extname: ".hbs",
@@ -25,17 +41,32 @@ app.engine(".hbs", exphbs.engine({
 app.set('views', path.join(__dirname, '../views'));
 
 // ==============================
+// MIDDLEWARE HELPER FUNCTIONS
+// ==============================
+app.use((req, res, next) => {
+    res.locals.user = req.session.user;
+    next();
+});
+
+// ==============================
 // PORT CONNECTION -- MONGODB HERE
 // ==============================
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Listening in PORT: ${PORT}`));
+connectToMongo()
+    .then(() => {
+        console.log('Connected to MongoDB...');
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => console.log(`Listening in PORT: ${PORT}`));
+    })
+    .catch((error) => {
+        console.error('Failed to connect to MongoDB', error);
+    });
 
 // ==============================
 // ROUTE HANDLERS
 // ==============================
 
 // HOME
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.render('index');
 });
 /*
@@ -44,15 +75,54 @@ app.get("/order", (req, res) => {
 });
 */
 
-app.get("/login", (req, res) => {
+app.get('/login', (req, res) => {
     res.render('loginPage');
 });
 
-app.get("/register", (req, res) => {
+app.post('/login', async (req, res) => {
+    const { identifier, password } = req.body;
+    try {
+        // Find user first with their EMAIL
+        let user = await User.findOne({ email: identifier });
+
+        // NO USER -- find restaurant with their USER
+        if (!user) {
+            user = await User.findOne({ username: identifier });
+            if (!user) {
+                return res.status(400).send({ error: 'Invalid credentials 1' });
+            }
+        } else {
+            console.log('USER FOUND');
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).send({ error: 'Invalid credentials 2' });
+        }
+
+        console.log('Credentials CORRECT!');
+
+        req.session.user = {
+            id: user._id,
+            email: user.email,
+            username: user.username,
+            name: user.name,
+            address: user.address,
+            phoneNumber: user.phoneNumber,
+            role: user.role,
+        };
+
+        res.redirect('/');
+    } catch (err) {
+        res.status(400).send({ error: 'Error logging in' });
+    }
+});
+
+app.get('/register', (req, res) => {
     res.render('registerPage.hbs');
 });
 
-app.get("/order", (req, res) => {
+app.get('/order', (req, res) => {
 
     // TODO: Should be from database (using mongoose)
     const dummyFoods = [
