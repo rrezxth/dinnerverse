c2d3230ce95da25b488bb2176d62b57c1fec26a8
rrezxth <rienz.edu@icloud.com>
2024-07-31 19:04:52 -0400

    Created model schema for orders

---
+++ src/database/model/orderModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+    user_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    restaurant_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Restaurant',
+        required: true,
+    },
+    items: [
+        {
+            item_id: {
+                type: Schema.Types.ObjectId,
+                ref: 'Item',
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            },
+            price: {
+                type: Number,
+                required: true,
+            }
+        }
+    ],
+    total_price: {
+        type: Number,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'confirmed', 'completed', 'canceled'],
+        default: 'pending',
+        required: true,
+    },
+    pickup_time: {
+        type: Date,
+        required: true,
+    }
+}, { timestamps: true });
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
